53dd1780d406450b115b6c60bf4714730d894c52
Sergio <lsergiobp@gmail.com>
2010-11-24 18:23:10 -0300

    Implementando Selecionar Oponente

---
+++ TicTacToe/src/business/ControlTicTacToe.java
@@ -0,0 +1,12 @@
+package business;
+
+import persistence.RulesTicTacToe;
+
+public class ControlTicTacToe {
+	
+	public static void setLevel(int level) {
+		RulesTicTacToe.setLevel(level);
+	}
+	
+
+}
---
+++ TicTacToe/src/persistence/RulesTicTacToe.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import java.util.Random;
+
+public class RulesTicTacToe {
+	private static int level;
+
+	public static void setLevel(int level) {
+		RulesTicTacToe.level = level;
+	}
+}
